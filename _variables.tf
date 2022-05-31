1203e73c9b90a6d3d929ad2d8a52ad0761ef6a14
Nick Tampakas <nick@gmx.co.uk>
2022-05-31 16:36:20 +0300

    store state in s3

--- _variables.tf
+++ _variables.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    region  = "eu-central-1"
+    profile = "devops"
+    bucket  = "privacyscaling-tf-state"
+    key     = "nt-eks.tfstate"
+    encrypt = "true"
+  }
+}
+
+
 locals {
   vpc = {
     name        = "nt-eks"
