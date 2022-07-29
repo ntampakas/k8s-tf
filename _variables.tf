7354bbc19ce8f431530f8a79fe5fc0b6b8affe97
Nick Tampakas <nick@gmx.co.uk>
2022-07-29 15:50:28 +0300

    Add basic files

--- _variables.tf
+++ _variables.tf
@@ -3,14 +3,23 @@ terraform {
     region  = "eu-central-1"
     profile = "devops"
     bucket  = "privacyscaling-tf-state"
-    key     = "nt-eks.tfstate"
+    key     = "nt-eks-v2.tfstate"
     encrypt = "true"
   }
 }
 
+variable "region" {
+  default     = "eu-central-1"
+  description = "AWS region"
+}
+
+provider "aws" {
+  region = var.region
+}
+
 locals {
   vpc = {
-    name        = "nt-eks"
-    cidr_prefix = "10.45"
+    name        = "nt-eks-v2"
+    cidr_prefix = "10.75"
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,50 +1,40 @@
-variable "region" {
-  default     = "eu-central-1"
-  description = "AWS region"
-}
-
-provider "aws" {
-  region = var.region
-}
-
 data "aws_availability_zones" "available" {}
-
 locals {
-  #cluster_name = "${local.vpc.name}-${random_string.suffix.result}"
   cluster_name = "${local.vpc.name}-dev"
 }
 
-#resource "random_string" "suffix" {
-#  length  = 4
-#  special = false
-#}
-
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.0"
+  source  = "aws-ia/vpc/aws"
+  version = ">= 1.0.0"
 
-  name                 = local.vpc.name
-  cidr                 = "${local.vpc.cidr_prefix}.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["${local.vpc.cidr_prefix}.1.0/24", "${local.vpc.cidr_prefix}.2.0/24", "${local.vpc.cidr_prefix}.3.0/24"]
-  public_subnets       = ["${local.vpc.cidr_prefix}.4.0/24", "${local.vpc.cidr_prefix}.5.0/24", "${local.vpc.cidr_prefix}.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  name                     = local.vpc.name
+  tags_prefix              = local.vpc.name
+  cidr_block               = "${local.vpc.cidr_prefix}.0.0/16"
+  az_count                 = length(data.aws_availability_zones.available.names)
+  vpc_enable_dns_hostnames = true
 
-  tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  }
+  subnets = {
+    public = {
+      cidrs = ["${local.vpc.cidr_prefix}.4.0/24", "${local.vpc.cidr_prefix}.5.0/24", "${local.vpc.cidr_prefix}.6.0/24"]
+      #nat_gateway_configuration = "all_azs"
+      nat_gateway_configuration = "single_az"
 
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-    "kubernetes.io/type"                          = "public"
-  }
+      tags = {
+        "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+        "kubernetes.io/role/elb"                      = "1"
+        "kubernetes.io/type"                          = "public"
+      }
+    }
 
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-    "kubernetes.io/type"                          = "private"
+    private = {
+      cidrs        = ["${local.vpc.cidr_prefix}.1.0/24", "${local.vpc.cidr_prefix}.2.0/24", "${local.vpc.cidr_prefix}.3.0/24"]
+      route_to_nat = true
+      tags = {
+        "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+        "kubernetes.io/role/internal-elb"             = "1"
+        "kubernetes.io/type"                          = "private"
+      }
+    }
+
   }
 }
